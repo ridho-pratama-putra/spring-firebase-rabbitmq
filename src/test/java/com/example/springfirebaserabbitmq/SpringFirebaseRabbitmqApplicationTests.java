b4b070b409dc956b4ee9d111d2ac40407ac21984
ridho <ridho.putra@btpn.com>
2023-01-23 21:32:09 +0700

    init

---
+++ src/test/java/com/example/springfirebaserabbitmq/SpringFirebaseRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfirebaserabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirebaseRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
